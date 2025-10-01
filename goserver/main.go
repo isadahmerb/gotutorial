2efafac11a44305212912082c4cea88186893dc7
isadahmerb <idahmerbarbosa@gmail.com>
2025-10-01 16:00:39 -0300

    fix

--- goserver/main.go
+++ goserver/main.go
@@ -33,7 +33,7 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 func main() {
 	fileServer := http.FileServer(http.Dir("./static"))
 	http.Handle("/", fileServer)
-	http.HandleFunc("/fomr", formHandler)
+	http.HandleFunc("/form", formHandler)
 	http.HandleFunc("/hello", helloHandler)
 
 	fmt.Printf("Starting server at port 8080\n")
